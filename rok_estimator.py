5ddd3fad97bbdcc1793fcf507cf22e306304884e
Russell W. F. Lai <russell.lai@aalto.fi>
2024-11-29 02:08:55 +0200

    pi_noop, pi_bdecomp, pi_split

--- rok_estimator.py
+++ rok_estimator.py
@@ -51,6 +51,7 @@ class RingParam:
     RingParam(f=60, phi=16, log_q=32, e=3, ring_exp_inf=16)
     """
     f: int = 1                                  # conductor
+    fhat: int = 1                               # fhat = f if f is odd and f/2 if f is even
     phi: int = field(init=False)                # degree
     log_q: int = 64                             # log of modulus q (assumed prime)
     e: int | None = None                        # inertia degree of q, i.e. R_q splits into fields of cardinality q**e 
@@ -63,90 +64,168 @@ class RingParam:
         self.phi = euler_phi(self.f)
         self.ring_exp_inf = euler_phi(self.f)
         if self.C == None:
-            self.C = SubtractiveSet()
+            self.C = SubtractiveSet.gen_klno24_cyclotomic(self.f)   # Use the subtractive set construction for cyclotomic fields reported in KLNO24
         if self.e == None:
-            self.e = ceil(80/self.log_q)
+            self.e = ceil(80/self.log_q)        # Aim for 80 bits of soundness for Schwartz-Zippel
+        if is_even(self.f):
+            self.fhat = self.f/2
+        else:
+            self.fhat = self.f
 
     def size_Rq(self):
         return self.phi * self.log_q
 
 @dataclass
-class Relation:
-    """Data class for relations."""
-    ncom: int = 1                   # module rank of commitment, with extractability guarantee
-    nrel_uncompressed: int = 1      # number of uncompressed relations, no extractability guarantee  
-    nrel_compressed: int = 1        # number of compressed relations, no extractability guarantee 
-    wdim: int = 1                   # dimension of each witness 
-    wbdl: int = 1                   # bundle size of witnesses 
-    log_beta_wit_2: float = 1       # log of canonical ell_2-norm bound of witness
-    log_beta_wit_inf: float = 1     # log of coefficient ell_inf-norm bound of witness
-    log_beta_ext_2: float = 1       # log of canonical ell_2-norm bound of witness
-    log_beta_ext_inf: float = 1     # log of coefficient ell_inf-norm bound of witness
-
-# The identity function
-var('x')
-id_expr = x
-id_f = id_expr.function(x)
+class Cost:
+    """
+    Data class for costs of reductions of knowledge.
+    
+    Example:
+    sage: Cost(beta_ext_2=1,beta_ext_inf=1,comm=0,snd=0)
+    """
+    beta_ext_2 : int = 1        # norm expansion factor for canonical ell_2-norm after extraction
+    beta_ext_inf : int = 1      # norm expansion factor for coefficient ell_inf-norm after extraction
+    comm : int = 0              # communication cost
+    snd : int = 0               # soundness cost
 
 @dataclass
-class RoK:
-    """Data class for reductions of knowledge."""
-    name: str       # name of RoK
-    f_nrel_uncompressed: Expression = id_f      # function for new nrel_uncompressed
-    f_nrel_compressed: Expression = id_f        # function for new nrel_compressed
-    f_wdim: Expression = id_f                   # function for new wdim
-    f_wbdl: Expression = id_f                   # function for new wbdl
-    f_log_beta_wit_2: Expression = id_f         # function for new log_beta_wit_2
-    f_log_beta_wit_inf: Expression = id_f       # function for new log_beta_wit_inf
-    f_log_beta_ext_2: Expression = id_f         # function for new log_beta_ext_2
-    f_log_beta_ext_inf: Expression = id_f       # function for new log_beta_ext_inf
-
-    # TODO: communication cost
-    # TODO: soundness cost
-
-pi_noop     = RoK(name="noop")      # the no-operation RoK for testing purposes
-pi_bdecomp  = RoK(name="bdecomp")   # TODO: set parameters
-pi_split    = RoK(name="split")     # TODO: set parameters
-pi_fold     = RoK(name="fold")      # TODO: set parameters
-pi_batch    = RoK(name="batch")     # TODO: set parameters
-pi_norm     = RoK(name="norm")      # TODO: set parameters
-pi_ip       = RoK(name="ip")        # TODO: set parameters
-pi_aut      = RoK(name="aut")       # TODO: set parameters
-pi_finish   = RoK(name="finish")    # TODO: set parameters
-
-class Protocol:
-    # A history is a list of states
-    # A state is a tuple consisting of 
-    # - a relation, 
-    # - the name of the protocol used to reach this state, 
-    # - the added communication cost, 
-    # - the accumulated communication cost, 
-    # - the added soundness error, 
-    # - the accumulated soundness error, 
-    # - the ell_2-canonical norm of the extracted witness (only set if pi_finish is run), 
-    # - the ell_inf-coefficient norm of the extracted witness (only set if pi_finish is run). 
+class Relation:
+    """
+    Data class for relations.
     
-    def execute(pi: RoK, rel: Relation):
+    Example:
+    sage: Relation(ntop=1,nbot=1,nout=1,wdim=1,rep=1,log_beta_wit_2=1,log_beta_wit_inf=1,log_beta_ext_2=1,log_beta_ext_inf=1)
+    """
+    ring_params: RingParam   # ring parameters
+    ntop: int = 1                                   # module rank of commitment
+    nbot: int = 1                                   # number of non-commitment relations  
+    nout: int = 1                                   # number of compressed relations, including both commitment and non-commitment relations
+    wdim: int = 1                                   # dimension of each witness 
+    rep: int = 1                                   # bundle size of witnesses 
+    log_beta_wit_2: float = 1                       # log of canonical ell_2-norm bound of witness
+    log_beta_wit_inf: float = 1                     # log of coefficient ell_inf-norm bound of witness
+    
+    def pi_noop(self):
         """
-        Execute RoK pi on relation rel.
+        Returns the relation resulting from the pi_noop RoK and its costs. 
+        
+        The RoK pi_noop does nothing. It is there for testing purposes.
+        """
+        return deepcopy(self), Cost()
+    
+    def pi_finish(self):
+        """
+        Returns the relation resulting from the pi_finish RoK and its costs. 
+        """
+        return deepcopy(self), Cost()
+      
+    def pi_bdecomp(self,base=None,ell=None):
+        """
+        Returns the relation resulting from the pi_bdecomp RoK and its costs. 
+        """
+        if base == None and ell == None:
+            raise Exception("Parameters base and ell cannot be both undefined.")
+        if base != None and ell != None:
+            raise Exception("Parameters base and ell cannot be both defined.")
+        if base != None:
+            ell = ceil(log(2*(2**self.log_beta_wit_inf)+1,base))
+        else:
+            base = ceil( (2*(2**self.log_beta_wit_inf)+1)**(1/ell) ) 
+            if base <= 1:
+                raise Exception("The choice of ell is impossible. Parameter base must be greater than 1.")
+            
+        rel = deepcopy(self)
+        rel.rep = self.rep * ell
+        rel.log_beta_ext_2      = log(sqrt(ell * self.rep * self.wdim * self.ring_params.fhat * self.ring_params.phi) * base / 2,2)
+        rel.log_beta_ext_inf    = log(floor(base / 2),2)
+        cost_beta_ext_2 = (base**ell-1)/(base-1)
+        cost_beta_ext_inf = (base**ell-1)/(base-1)
+        cost_comm = self.ring_params.size_Rq() * (ell-1) * self.nout * self.rep
+        cost_snd = 0
+        cost = Cost(beta_ext_2=cost_beta_ext_2,beta_ext_inf=cost_beta_ext_inf,comm=cost_comm,snd=cost_snd)
+        return rel, cost
+    
+    def pi_split(self,d):
+        """     
+        Returns the relation resulting from the pi_split RoK and its costs.
+        """
+        if not d.divides(self.wdim):
+            raise Exception("Cannot split the witness into d chunks. Parameter d must divide wdim.")
+        
+        rel = deepcopy(self)
+        rel.wdim = self.wdim / d
+        rel.rep = self.rep * d
+        cost_beta_ext_2 = 1
+        cost_beta_ext_inf = 1
+        cost_comm = self.ring_params.size_Rq() * ((d - 1) * self.ntop + (d**2 - 1) * (self.nout - self.ntop)) * self.rep
+        cost_snd = 0
+        cost = Cost(beta_ext_2=cost_beta_ext_2,beta_ext_inf=cost_beta_ext_inf,comm=cost_comm,snd=cost_snd)
+        return rel, cost
+    
+    def pi_fold(self):
+        """
+        Returns the relation resulting from the pi_fold RoK and its costs. 
+        """
+        return deepcopy(self), Cost()
+    
+    def pi_batch(self):
+        """
+        Returns the relation resulting from the pi_batch RoK and its costs. 
+        """
+        return deepcopy(self), Cost()
+    
+    def pi_norm(self):
+        """
+        Returns the relation resulting from the pi_norm RoK and its costs. 
+        """
+        return deepcopy(self), Cost()
+    
+    def pi_ip(self):
+        """
+        Returns the relation resulting from the pi_ip RoK and its costs. 
+        """
+        return deepcopy(self), Cost()
+    
+    def pi_aut(self):
+        """
+        Returns the relation resulting from the pi_aut RoK and its costs. 
+        """
+        return deepcopy(self), Cost()
+    
 
-        Example:
-        sage: rel = Relation()
-        sage: new_rel = execute(pi_noop, rel)
-        """
+# class Protocol:
+#     # A history is a list of states
+#     # A state is a tuple consisting of 
+#     # - a relation, 
+#     # - the name of the protocol used to reach this state, 
+#     # - the added communication cost, 
+#     # - the accumulated communication cost, 
+#     # - the added soundness error, 
+#     # - the accumulated soundness error, 
+#     # - the ell_2-canonical norm of the extracted witness (only set if pi_finish is run), 
+#     # - the ell_inf-coefficient norm of the extracted witness (only set if pi_finish is run). 
+    
+#     def execute(pi: RoK, rel: Relation):
+#         """
+#         Execute RoK pi on relation rel.
+
+#         Example:
+#         sage: rel = Relation()
+#         sage: new_rel = execute(pi_noop, rel)
+#         """
         
-        # TODO: Raise exception if any protocol is run after pi_finish.
+#         # TODO: Raise exception if any protocol is run after pi_finish.
         
-        rel.nrel_uncompressed = pi.f_nrel_uncompressed(rel.nrel_uncompressed)
-        rel.nrel_compressed = pi.f_nrel_compressed(rel.nrel_compressed)
-        rel.wdim = pi.f_wdim(rel.wdim)
-        rel.wbdl = pi.f_wbdl(rel.wbdl)
-        rel.log_beta_wit_2 = pi.f_log_beta_wit_2(rel.log_beta_wit_2)
-        rel.log_beta_wit_inf = pi.f_log_beta_wit_inf(rel.log_beta_wit_inf)
-        rel.log_beta_ext_2 = pi.f_log_beta_ext_2(rel.log_beta_ext_2)
-        rel.log_beta_ext_inf = pi.f_log_beta_ext_inf(rel.log_beta_ext_inf)
+#         rel.nbot = pi.f_nbot(rel.nbot)
+#         rel.nout = pi.f_nout(rel.nout)
+#         rel.wdim = pi.f_wdim(rel.wdim)
+#         rel.rep = pi.f_rep(rel.rep)
+#         rel.log_beta_wit_2 = pi.f_log_beta_wit_2(rel.log_beta_wit_2)
+#         rel.log_beta_wit_inf = pi.f_log_beta_wit_inf(rel.log_beta_wit_inf)
+#         rel.log_beta_ext_2 = pi.f_log_beta_ext_2(rel.log_beta_ext_2)
+#         rel.log_beta_ext_inf = pi.f_log_beta_ext_inf(rel.log_beta_ext_inf)
         
-        # TODO: Keep track of communication cost. 
-        # TODO: Keep track of soundness cost. 
+#         # TODO: Keep track of communication cost. 
+#         # TODO: Keep track of soundness cost. 
         
-        return rel      # TODO: return communication and soundness costs
+#         return rel      # TODO: return communication and soundness costs
